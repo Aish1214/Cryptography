a354de6512cbc15c9f06c9a69126a3fe356ffe42
Aish1214 <funaguskaraishwarya@gmail.com>
2023-03-10 19:11:20 +0600

    Add files via upload

---
+++ Caesar_Decryption.py
@@ -0,0 +1,19 @@
+def encrypt(text,s):
+   
+    # Cipher(n) = De-cipher(26-n)
+    s=s   
+    text =text.replace(" ","")
+    result=""  #empty string
+    for i in range(len(text)):
+        char=text[i]
+        if(char.isupper()):  #if the text[i] is in upper case
+            result=result+chr((ord(char)+s-65)%26+65)
+        else:
+            result=result+chr((ord(char)+s-97)%26+97)
+    return result
+
+
+word=str(input("enter the word:"))
+k=int(input("Enter the key: "))
+
+print("Encoded word in Caeser cipher is: ",encrypt(word,k))
